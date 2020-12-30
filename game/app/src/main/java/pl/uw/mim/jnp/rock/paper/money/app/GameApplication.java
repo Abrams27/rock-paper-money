bca60f06edfeb2a3e78ccecf5f97091128f32a4b
Abrams27 <abramowiczmarcin99@gmail.com>
2020-12-30 16:17:39 +0100

    game modules

---
+++ game/app/src/main/java/pl/uw/mim/jnp/rock/paper/money/app/GameApplication.java
@@ -0,0 +1,12 @@
+package pl.uw.mim.jnp.rock.paper.money.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameApplication.class, args);
+  }
+}
