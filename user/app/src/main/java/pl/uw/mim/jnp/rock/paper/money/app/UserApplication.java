2ef4d6bac9ebac1c0ffc35c90b827650afa53ad0
Abrams27 <abramowiczmarcin99@gmail.com>
2020-12-30 15:58:01 +0100

    user modules

---
+++ user/app/src/main/java/pl/uw/mim/jnp/rock/paper/money/app/UserApplication.java
@@ -0,0 +1,12 @@
+package pl.uw.mim.jnp.rock.paper.money.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserApplication.class, args);
+  }
+}
