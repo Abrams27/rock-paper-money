1a5c656e6512cd34ac4c53119916a79c90878ba8
Abrams27 <abramowiczmarcin99@gmail.com>
2021-02-15 13:01:55 +0100

    [deploy] chmod + file name udpate

--- k8s/env/kafka/deploy.sh
+++ k8s/env/kafka/deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl apply -f zookeeper.deployment.yaml
-kubectl apply -f zookeeper.service.yaml
+kubectl apply -f zookeeper.deployment.yml
+kubectl apply -f zookeeper.service.yml
 
-kubectl apply -f kafka.service.yaml
-kubectl apply -f kafka.deployment.yaml
-
-
+kubectl apply -f kafka.service.yml
+kubectl apply -f kafka.deployment.yml
